8b7fd282a0ba2b4a29f64b1a507f54c092a32358
Tim Kerschbaumer <tim.kersch@gmail.com>
2015-05-16 14:37:13 +0200

    m

--- SnapSpot/app/src/main/java/edit/com/snapspot/ui/MainActivity.java
+++ SnapSpot/app/src/main/java/edit/com/snapspot/ui/MainActivity.java
@@ -219,7 +219,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     public void getPOIs(){
         // Todo: Use callback to get POIs
-        DbOperations.getSpots(this, new POICallback() {
+        DbOperations.getSpots(new POICallback() {
             @Override
             public void onPOIReady(List<Spot> spots) {
                 // Clear all markers
