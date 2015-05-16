b91058f3f484923fb9da53ad04caf4e8609983ae
Tim Kerschbaumer <tim.kersch@gmail.com>
2015-05-16 09:53:57 +0200

    Updates in DbOperations

--- SnapSpot/app/src/main/java/edit/com/snapspot/appEngineServices/DbOperations.java
+++ SnapSpot/app/src/main/java/edit/com/snapspot/appEngineServices/DbOperations.java
@@ -8,9 +8,12 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import edit.com.backend.registration.Registration;
+import edit.com.backend.spot.model.CollectionResponseSpotRecord;
+import edit.com.backend.spot.model.SpotRecord;
 import edit.com.snapspot.models.Spot;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,16 +32,70 @@ public class DbOperations {
 	private static final String PROJECT_ID = "461803206887";
 
 	private static Registration regService;
+	private static edit.com.backend.spot.Spot spotService;
 	private static GoogleCloudMessaging gcm;
 
-	public static void addSpot(Spot spot) {
+	/** Adds a spot to the backend
+	 * @param spot the spot to be added
+	 */
+	public static void addSpot(final Spot spot) {
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				initSpotService();
+				try {
+					spotService.addSpot(spot.getName(), spot.getDescription(), spot.getLatitude(), spot.getLongitude());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		}.execute();
 	}
 
-	public static void deleteSpot(Spot spot) {
+	/** Deletes a spot from the backed
+	 * @param spot the spot to be deleted
+	 */
+	public static void deleteSpot(final Spot spot) {
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				initSpotService();
+				try {
+					spotService.removeSpot(spot.getName());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		}.execute();
 	}
 
-	public static List<Spot> getSpots() {
-		return null;
+	public static void getSpots() {
+		new AsyncTask<Void, Void, List<Spot>>() {
+			@Override
+			protected List<Spot> doInBackground(Void... params) {
+				initSpotService();
+				List<Spot> spotList = null;
+				try {
+					CollectionResponseSpotRecord record = spotService.getSpots().execute();
+					List<SpotRecord> spots = record.getItems();
+					spotList = new ArrayList<Spot>(spots.size());
+					for(SpotRecord s : spots) {
+						spotList.add(new Spot(s.getGeoPt().getLatitude(), s.getGeoPt().getLongitude(), s.getName(), s.getDescription()));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return spotList;
+			}
+
+			@Override
+			protected void onPostExecute(List<Spot> spots) {
+				// Callback
+			}
+
+		}.execute();
 	}
 
 	/** Register this device for GCM
@@ -67,6 +124,28 @@ public class DbOperations {
 				// Store the registration ID
 			}
 		}.execute();
+	}
+
+	// Initialize the spot service
+	private static synchronized void initSpotService() {
+		if(spotService == null) {
+			edit.com.backend.spot.Spot.Builder builder;
+			if(LOCAL) {
+				// Code to run on LOCAL machine
+				builder = new edit.com.backend.spot.Spot.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+						.setRootUrl("http://localhost:8080/_ah/api")
+						.setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+							@Override
+							public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+								abstractGoogleClientRequest.setDisableGZipContent(true);
+							}
+						});
+			} else {
+				builder = new edit.com.backend.spot.Spot.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+						.setRootUrl("https://southern-shard-93711.appspot.com/_ah/api/");
+			}
+			spotService = builder.build();
+		}
 	}
 
 	// Initialize the registration service for gcm
